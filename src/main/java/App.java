26fa0032ff7b09f9f4fb3457a63d35e856c06141
2.1.3_Spring_HelloWorld <Dmitriyfromrussia@gmail.com>
2024-07-20 23:58:19 +0200

    Добрый день. В Шестой строке класса App оставил комментарий. Прошу подсказать, о чем почитать, чтобы смог найти ответ на вопрос

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        ApplicationContext applicationContext = // не понятно, почему не написать AnnotationConfigApplicationContext вместо ApplicationContext
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean2));
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getVoice());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3 == bean4);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    private String voice;
+
+    public Cat() { // можно не создавать конструктор
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+}
